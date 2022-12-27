9f1fba3017fb479d5e57f0d25e0ce4cf2315f6ed
Michael Kumsa <michael.kumsa@astu.edu.et>
2022-12-27 02:18:07 -0800

    Created Views, Serializers and Util.py

--- inventory_api/inventory_api/settings.py
+++ inventory_api/inventory_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x%kyxe)ekt%wabtkpjovnc0kx)n3j$yk-vu=m-ju6spf2*hm-c'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
