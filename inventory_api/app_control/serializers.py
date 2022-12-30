49af97bf8080d87a2537dd0ac93ca2a5d2bab70a
Michael Kumsa <michael.kumsa@astu.edu.et>
2022-12-30 14:50:23 -0800

    updated views.py from the app_control

--- inventory_api/app_control/serializers.py
+++ inventory_api/app_control/serializers.py
@@ -22,19 +22,11 @@ class InventoryGroupSerializer(serializers.ModelSerializer):
 
 
 class Inventory(serializers.ModelSerializer):
-    user = CustomUserSerializer(read_only=True)
     created_by = serializers.CharField(write_only=True)
-    belongs_to = serializers.SerializerMethodField(read_only=True)
-    belongs_to_id = serializers.CharField(write_only=True)
-    total_items = serializers.CharField(read_only=True, required=False)
+    created_by_id = serializers.CharField(write_only=True, required=False)
     group = InventoryGroupSerializer(read_only=True)
     group_id = serializers.CharField(write_only=True)
 
     class Meta:
         model = Inventory
         fields = '__all__'
-
-    def get_belongs_to(self, obj):
-        if obj.belongs_to is not None:
-            return InventoryGroupSerializer(obj.belongs_to).data
-        return None
--- inventory_api/app_control/views.py
+++ inventory_api/app_control/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import InventoryGroupSerializer, InventorySerializer, InventoryGroup, Inventory
+from rest_framework.response import Response
+from inventory_api.custom_methods import IsAuthenticatedCustom
 
-# Create your views here.
+
+class InventoryView(ModelViewSet):
+    queryset = Inventory.objects.select_related('group', 'created_by')
+    serializer_class = InventorySerializer
+    permission_classes = [IsAuthenticatedCustom]
+
+    def create(self, request, *args, **kwargs):
+        request.data.update({'created_by_id': request.user.id})
+        return super().create(request, *args, **kwargs)
+
+class InventoryGroupView(ModelViewSet):
+    queryset = InventoryGroup.objects.select_related('belongs_to', 'created_by').prefetch_related('inventories')
+    serializer_class = InventoryGroupSerializer
+
+    def create(self, request, *args, **kwargs):
+        request.data.update({'created_by_id': request.user.id})
+        return super().create(request, *args, **kwargs)
