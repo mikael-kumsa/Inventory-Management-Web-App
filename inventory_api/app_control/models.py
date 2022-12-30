8e722a3853671e151472c3ad1a9199371812e26a
Michael Kumsa <michael.kumsa@astu.edu.et>
2022-12-30 10:52:59 -0800

    Updated models.py from app_control

--- inventory_api/app_control/models.py
+++ inventory_api/app_control/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from user_control.models import CustomUser
-
+from user_control.views import add_user_activity
 # Create your models here.
 
 class InventoryGroup(models.Model):
@@ -14,5 +14,35 @@ class InventoryGroup(models.Model):
     class Meta:
         ordering = ("-created_at", )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.old_name = self.name
+
     def save(self, *args, **kwargs):
         action = f"Created Inventory Group: {self.name}"
+        if self.pk is not None:
+            action = f"Updated Inventory Group from '{self.old_name}' to '{self.name}'"
+        super().save(*args, **kwargs)
+
+        add_user_activity(self.created_by, action)
+
+    def delete(self, *args, **kwargs):
+        created_by = self.created_by
+        action = f"Deleted Inventory Group: {self.name}"
+        super().delete(*args, **kwargs)
+        add_user_activity(created_by, action)
+
+    def __str__(self):
+        return self.name
+
+class Inventory(models.Model):
+    created_by = models.ForeignKey(CustomUser, null=True, related_name="inventory_groups",on_delete=models.SET_NULL)
+    code = models.CharField(max_length=100, unique=True, null=True)
+    photo = models.TextField(blank=True, null=True)
+    groups = models.ForeignKey(InventoryGroup, related_name="inventory_groups", null=True, on_delete=models.SET_NULL)
+    total = models.PositiveIntegerField()
+    remaining = models.PositiveIntegerField(null=True)
+    name = models.CharField(max_length=256)
+    price = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
