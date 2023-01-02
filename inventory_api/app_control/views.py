44c51c2adba13f016ab1004043fe1223d5d3c0a2
Michael Kumsa <michael.kumsa@astu.edu.et>
2023-01-01 23:30:02 -0800

    updated views

--- inventory_api/app_control/views.py
+++ inventory_api/app_control/views.py
@@ -3,7 +3,7 @@ from .serializers import InventoryGroupSerializer, InventorySerializer, Inventor
 from inventory_api.custom_methods import IsAuthenticatedCustom
 from inventory_api.utils import CustomPagination, get_query
 from django.db.models import Count
-
+from user_control.models import CustomUser
 class InventoryView(ModelViewSet):
     queryset = Inventory.objects.select_related('group', 'created_by')
     serializer_class = InventorySerializer
@@ -110,3 +110,23 @@ class InvoiceView(ModelViewSet):
     def create(self, request, *args, **kwargs):
         request.data.update({'created_by_id': request.user.id})
         return super().create(request, *args, **kwargs)
+
+class SummaryView(ModelViewSet):
+    http_method_names = ('get',)
+    permission_classes = [IsAuthenticatedCustom]
+    queryset = InventoryView.queryset
+
+    def list(self, request, *args, **kwargs):
+        total_inventories = InventoryView.queryset.filter(
+            remaining_gt=0
+        ).count()
+        total_group = InventoryGroupView.queryset.count()
+        total_shop = ShopView.queryset.count()
+        total_user = CustomUser.objects.filter(is_superuser=False).count()
+
+        return Response({
+            "total_inventory": total_inventories,
+            "total_group": total_group,
+            "total_shop": total_shop,
+            "total_user": total_user
+        })
