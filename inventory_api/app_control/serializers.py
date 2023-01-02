4834f263e47d155855fbacd770b502d01406dd8e
Michael Kumsa <michael.kumsa@astu.edu.et>
2023-01-02 01:11:34 -0800

    updated serializers.py

--- inventory_api/app_control/serializers.py
+++ inventory_api/app_control/serializers.py
@@ -21,7 +21,7 @@ class InventoryGroupSerializer(serializers.ModelSerializer):
         return None
 
 
-class Inventory(serializers.ModelSerializer):
+class InventorySerializer(serializers.ModelSerializer):
     created_by = serializers.CharField(write_only=True)
     created_by_id = serializers.CharField(write_only=True, required=False)
     group = InventoryGroupSerializer(read_only=True)
@@ -30,6 +30,10 @@ class Inventory(serializers.ModelSerializer):
     class Meta:
         model = Inventory
         fields = '__all__'
+
+class InventoryWithSumSerializer(InventorySerializer):
+    sum_of_item = serializers.IntegerField()
+
 
 class ShopSerializer(serializers.ModelSerializer):
     created_by = CustomUserSerializer(read_only=True)
@@ -40,6 +44,10 @@ class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
         fields = '__all__'
+
+class ShopWithAmountSerializer(ShopSerializer):
+    amount_total = serializers.FloatField()
+    month = serializers.CharField(required=False)
 
 class InvoiceItemSerializer(serializers.ModelSerializer):
     invoice = serializers.CharField(read_only=True)
--- inventory_api/app_control/views.py
+++ inventory_api/app_control/views.py
@@ -1,9 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializers import InventoryGroupSerializer, InventorySerializer, InventoryGroup, Inventory, ShopSerializer, Shop, Invoice, InvoiceSerializer
-from inventory_api.custom_methods import IsAuthenticatedCustom
+from .serializers import (InventoryGroupSerializer, InventorySerializer, InventoryGroup,
+                         Inventory, ShopSerializer, Shop, Invoice, InvoiceSerializer, InvoiceItem,
+                          InventoryWithSumSerializer, ShopWithAmountSerializer)
 from inventory_api.utils import CustomPagination, get_query
-from django.db.models import Count
+from django.db.models import Count, Sum, F
+from django.db.models.functions import Coalesce, TruncMonth
 from user_control.models import CustomUser
+from rest_framework.response import Response
+from inventory_api.custom_methods import IsAuthenticatedCustom
+
+
 class InventoryView(ModelViewSet):
     queryset = Inventory.objects.select_related('group', 'created_by')
     serializer_class = InventorySerializer
@@ -130,3 +136,62 @@ class SummaryView(ModelViewSet):
             "total_shop": total_shop,
             "total_user": total_user
         })
+
+
+class SalePerformanceView(ModelViewSet):
+    http_method_names = ('get',)
+    permission_classes = [IsAuthenticatedCustom]
+    queryset = InventoryView.queryset
+
+    def list(self, request, *args, **kwargs):
+        query_data = request.query_params.dict()
+        total = query_data.get("total", None)
+        query = self.queryset
+
+        if not total:
+            start_date = query_data.get("start_date", None)
+            end_date = query_data.get("end_date", None)
+            if start_date:
+                query = query.filter(
+                inventory_invoice__created_at__range = [start_date, end_date]
+            )
+        
+        items = query.annotate(
+            sum_of_item = Coalesce(Sum('inventory_invoices__quantity'), 0)
+        ).order_by('-sum_of_item')[:10]
+
+        response_data = InventoryWithSumSerializer(items, many=True).data
+        return Response(response_data)
+
+class SaleByShopView(ModelViewSet):
+    http_method_names = ('get',)
+    permission_classes = [IsAuthenticatedCustom]
+    queryset = InventoryView.queryset
+
+    def list(self, request, *args, **kwargs):
+        query_data = request.query_params.dict()
+        total = query_data.get("total", None)
+        monthly = query_data.get("monthly", None)
+        query = ShopView.queryset
+
+        if not total:
+            start_date = query_data.get("start_date", None)
+            end_date = query_data.get("end_date", None)
+            if start_date:
+                query = query.filter(
+                sale_shop__created_at__range = [start_date, end_date]
+            )
+
+        if monthly:
+            shops = query.annotate(month = TruncMonth('created_at')).values('month', 'name').annotate(
+                amount_total = sum(F("sale_shop__invoice_items__quantity") * 
+                                   F("sale_shop__invoice_items__amount")
+                                   )) 
+        else:
+            shops = query.annotate(
+                amount_total = sum(F("sale_shop__invoice_items__quantity") * 
+                                   F("sale_shop__invoice_items__amount")
+                                   )).order_by('-amount_total')
+
+        response_data = ShopWithAmountSerializer(shops, many=True).data
+        return Response(response_data)
